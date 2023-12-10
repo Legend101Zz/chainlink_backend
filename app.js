34706f510dca7ea218826bb9fc9624a741b701d6
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-12-11 02:00:58 +0530

    adpi complete

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const { exec } = require("child_process");
 const app = express();
 const port = 3000;
 
-app.get("memex/reddit/:subreddit/:postId", (req, res) => {
+app.get("/test", (req, res) => {
+  res.send("Test route hit!");
+});
+
+app.get("/memex/reddit/:subreddit/:postId", (req, res) => {
   const { subreddit, postId } = req.params;
 
-  exec(`reddit_script.py ${subreddit} ${postId}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      res.status(500).send("Internal Server Error");
-      return;
+  exec(
+    `python3 reddit_script.py ${subreddit} ${postId}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      console.log(`Python Output: ${stdout}`);
+      res.send(`Python Output: ${stdout}`);
     }
-    console.log(`Python Output: ${stdout}`);
-    res.send(`Python Output: ${stdout}`);
-  });
+  );
 });
 
 app.listen(port, () => {
