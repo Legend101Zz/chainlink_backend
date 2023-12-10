91571ea9e185cac91c9aa4ecc50ab78bcb7b7348
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-12-11 01:54:59 +0530

    chnages in python script

--- app.js
+++ app.js
@@ -3,21 +3,18 @@ const { exec } = require("child_process");
 const app = express();
 const port = 3000;
 
-app.get("/:subreddit/:postId", (req, res) => {
+app.get("memex/reddit/:subreddit/:postId", (req, res) => {
   const { subreddit, postId } = req.params;
 
-  exec(
-    `python your_script.py ${subreddit} ${postId}`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error: ${error.message}`);
-        res.status(500).send("Internal Server Error");
-        return;
-      }
-      console.log(`Python Output: ${stdout}`);
-      res.send(`Python Output: ${stdout}`);
+  exec(`reddit_script.py ${subreddit} ${postId}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      res.status(500).send("Internal Server Error");
+      return;
     }
-  );
+    console.log(`Python Output: ${stdout}`);
+    res.send(`Python Output: ${stdout}`);
+  });
 });
 
 app.listen(port, () => {
--- reddit_script.py
+++ reddit_script.py
@@ -1,4 +1,8 @@
 import praw
+import sys
+
+subreddit_name = sys.argv[1]
+post_id = sys.argv[2]
 
 # Create a Reddit instance
 reddit = praw.Reddit(
@@ -7,9 +11,8 @@ reddit = praw.Reddit(
     user_agent='chainlink',
 )
 
-# Access the Chainlink subreddit and a specific post
-subreddit = reddit.subreddit('Chainlink')
-post_id = '18dscwy'
+# Access the specified subreddit and post
+subreddit = reddit.subreddit(subreddit_name)
 post = reddit.submission(id=post_id)
 
 # Access information about the post
